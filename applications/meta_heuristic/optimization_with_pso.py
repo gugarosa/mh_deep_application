57d737d1e7a8637970b63e695f7b68912d8befed
Gustavo Rosa <gth.rosa@uol.com.br>
2021-07-07 16:01:16 -0300

    Adding initial set of meta_heuristic applications.

---
+++ applications/meta_heuristic/optimization_with_pso.py
@@ -0,0 +1,31 @@
+import numpy as np
+from opytimark.markers.n_dimensional import Sphere
+from opytimizer import Opytimizer
+from opytimizer.core import Function
+from opytimizer.optimizers.swarm import PSO
+from opytimizer.spaces import SearchSpace
+
+# Random seed for experimental consistency
+np.random.seed(0)
+
+# Number of agents and decision variables
+n_agents = 20
+n_variables = 50
+
+# Lower and upper bounds (has to be the same size as `n_variables`)
+lower_bound = np.full(n_variables, -10)
+upper_bound = np.full(n_variables, 10)
+
+# Creates the space, optimizer and function
+space = SearchSpace(n_agents, n_variables, lower_bound, upper_bound)
+optimizer = PSO()
+function = Function(Sphere())
+
+# Bundles every piece into Opytimizer class
+opt = Opytimizer(space, optimizer, function, save_agents=False)
+
+# Runs the optimization task
+opt.start(n_iterations=250)
+
+# Saves the optimization model
+opt.save('standard_opt_pso.pkl')
