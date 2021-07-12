e5bb724220fff05d5a85ab541cf34457fffd7b64
Gustavo Rosa <gth.rosa@uol.com.br>
2021-07-12 14:37:36 -0300

    Adding script to find the best CNN hyperparameters.

--- applications/deep_learning/models/cnn.py
+++ applications/deep_learning/models/cnn.py
@@ -9,13 +9,16 @@ class ResNet(Model):
     """
 
     def __init__(self, n_channels=3, n_output=10, lr=0.001,
-                 init_weights=None, device='cpu'):
+                 kernel_size=3, stride=1, padding=1, init_weights=None, device='cpu'):
         """Initialization method.
 
         Args:
             n_channels (int): Number of input channels.
             n_output (int): Number of output units.
             lr (float): Learning rate.
+            kernel_size (int): Size of first convolutional kernel.
+            stride (int): First convolutional layer stride.
+            padding (int): First convolutional layer padding.
             init_weights (tuple): Tuple holding the minimum and maximum values for weights initialization.
             device (str): Device that model should be trained on, e.g., `cpu` or `cuda`.
 
@@ -29,7 +32,7 @@ class ResNet(Model):
 
         # Replaces first convolutional layer with smaller kernel, stride and padding
         # (n_input, n_output, kernel_size, stride, padding)
-        self.model.conv1 = nn.Conv2d(n_channels, 64, 3, 1, 1, bias=False)
+        self.model.conv1 = nn.Conv2d(n_channels, 64, kernel_size, stride, padding, bias=False)
 
         # Replaces fully-connected layer with proper number of classes
         # (n_input, n_output)
---
+++ applications/hyperparameter_optimization/finding_best_cnn.py
@@ -0,0 +1,65 @@
+import sys
+sys.path.append('applications/deep_learning')
+
+import numpy as np
+import torch
+import torchvision
+from models import ResNet
+from opytimizer import Opytimizer
+from opytimizer.core import Function
+from opytimizer.optimizers.evolutionary import GA
+from opytimizer.spaces import SearchSpace
+from torch.utils.data import DataLoader
+
+
+def neural_network(x):
+    # Gathers current optimization parameters
+    kernel_size = int(x[0][0])
+    stride = int(x[1][0])
+    padding = int(x[2][0])
+
+    # Input transform
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor()
+    ])
+
+    # Loads training and validation sets
+    train = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    val = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+    # Creates training and validation loaders
+    train_loader = DataLoader(train, batch_size=100, shuffle=True, num_workers=0)
+    val_loader = DataLoader(val, batch_size=100, shuffle=False, num_workers=0)
+
+    # Instantiates the model
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = ResNet(n_channels=3, n_output=10, lr=0.001, kernel_size=kernel_size,
+                   stride=stride, padding=padding, device=device)
+
+    # Fits the model
+    _, _, _, val_acc = model.fit(train_loader, val_loader, epochs=3)
+
+    return 1 - val_acc
+
+# Random seeds for experimental consistency
+torch.manual_seed(0)
+np.random.seed(0)
+
+# Number of agents and decision variables
+n_agents = 5
+n_variables = 3
+
+# Lower and upper bounds (has to be the same size as `n_variables`)
+lower_bound = [1, 1, 1]
+upper_bound = [7, 5, 3]
+
+# Creates the space, optimizer and function
+space = SearchSpace(n_agents, n_variables, lower_bound, upper_bound)
+optimizer = GA()
+function = Function(neural_network)
+
+# Bundles every piece into Opytimizer class
+opt = Opytimizer(space, optimizer, function, save_agents=False)
+
+# Runs the optimization task
+opt.start(n_iterations=10)
